1bb68758f15fccc80b6915d3ffc05265fb0978e5
khanhdoan-2809 <khanhdoan21.work@gmail.com>
2024-12-28 21:30:29 +0700

    update

--- BE/Blogs.API/Blogs.API/obj/Release/net8.0/Blogs.API.AssemblyInfo.cs
+++ BE/Blogs.API/Blogs.API/obj/Release/net8.0/Blogs.API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blogs.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a82b24c04ee56a46b88edfd6cb719a52eddf5eb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blogs.API")]
